62a235b92a48fa522c7695fa294bbca625e33260
Lê Bảo Anh <60655387+b1506704@users.noreply.github.com>
2021-01-28 01:08:47 +0700

    hide mongodb uri

--- server/index.js
+++ server/index.js
@@ -13,8 +13,6 @@ app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
 app.use('/posts', postRoutes);
-// const CONNECTION_URL = process.env.CONNECTION_URL || 'mongodb+srv://b1506704:5969_Dante@eacm.t4tal.mongodb.net/EnglishActiveClassManagement?retryWrites=true&w=majority';
-// const CONNECTION_URL = 'mongodb+srv://b1506704:5969_Dante@eacm.t4tal.mongodb.net/EnglishActiveClassManagement?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
